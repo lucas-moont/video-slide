28052ad84ec1fa97040683ba9c73c537d3a1a6a2
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2023-11-24 17:23:25 -0300

    Video slide is working. Still needs some styles in the buttons.

--- script.js
+++ script.js
@@ -1,48 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const carousel = document.querySelector('.customVideoCarousel');
-    const carouselItems = carousel.querySelectorAll('.customVideoCarouselItem');
-    const prevBtn = carousel.querySelector('.customPrevSlide');
-    const nextBtn = carousel.querySelector('.customNextSlide');
-    let currentIndex = 0;
-  
-    function showVideo(index) {
-      carouselItems.forEach((item, i) => {
-        if (i === index) {
-          item.style.display = 'block'; // Exibe o vídeo atual
-        } else {
-          item.style.display = 'none'; // Esconde os demais vídeos
-        }
-      });
+  const carousel = document.querySelector('.customVideoCarousel');
+  const carouselItems = carousel.querySelector('.customVideoCarouselInner');
+  const prevBtn = carousel.querySelector('.customPrevSlide');
+  const nextBtn = carousel.querySelector('.customNextSlide');
+  let currentIndex = 0;
+
+  function updateButtonsVisibility() {
+    prevBtn.style.display = currentIndex === 0 ? 'none' : 'block';
+    nextBtn.style.display = currentIndex === carouselItems.children.length - 1 ? 'none' : 'block';
+  }
+
+  prevBtn.addEventListener('click', function () {
+    if (currentIndex > 0) {
+      currentIndex--;
+      const translateValue = currentIndex * carouselItems.children[0].clientWidth;
+      carouselItems.style.transform = `translateX(-${translateValue}px)`;
+      updateButtonsVisibility();
     }
-  
-    // Exibir o primeiro vídeo ao carregar a página
-    showVideo(currentIndex);
-  
-    prevBtn.addEventListener('click', function () {
-      if (currentIndex > 0) {
-        currentIndex--;
-        showVideo(currentIndex);
-      }
-    });
-  
-    nextBtn.addEventListener('click', function () {
-      if (currentIndex < carouselItems.length - 1) {
-        currentIndex++;
-        showVideo(currentIndex);
-      }
-    });
-  
-    // Ocultar botões de navegação ao atingir o início ou fim do carrossel
-    function toggleButtonsVisibility() {
-      prevBtn.style.visibility = currentIndex === 0 ? 'hidden' : 'visible';
-      nextBtn.style.visibility = currentIndex === carouselItems.length - 1 ? 'hidden' : 'visible';
-    }
-  
-    // Atualizar visibilidade dos botões ao carregar a página
-    toggleButtonsVisibility();
-  
-    // Atualizar visibilidade dos botões ao mudar de vídeo
-    prevBtn.addEventListener('click', toggleButtonsVisibility);
-    nextBtn.addEventListener('click', toggleButtonsVisibility);
   });
-  
+
+  nextBtn.addEventListener('click', function () {
+    if (currentIndex < carouselItems.children.length - 1) {
+      currentIndex++;
+      const translateValue = currentIndex * carouselItems.children[0].clientWidth;
+      carouselItems.style.transform = `translateX(-${translateValue}px)`;
+      updateButtonsVisibility();
+    }
+  });
+
+  updateButtonsVisibility();
+});
